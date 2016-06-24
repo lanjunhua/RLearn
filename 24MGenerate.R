c2e2a499c9273550bbfe138230a46ab6e91dae8f
lanjunhua <lanjunhua@users.noreply.github.com>
2016-06-24 15:42:49 +0800

    Create 24MGenerate.R

---
+++ 24MGenerate.R
@@ -0,0 +1,57 @@
+msPbocRecall24M <- function(TIMEL_LSTINQ, TIMEL_ISS, RCD_RP24M, TIME_NUM_DQ, monthOffSet){
+  if(TIMEL_ISS > TIMEL_LSTINQ - months(monthOffSet)) return(NA)
+  if(TIMEL_ISS < TIMEL_LSTINQ - months(monthOffSet)){
+    if(monthOffSet < 24){
+      
+      remained24MStr <- unlist(strsplit(RCD_RP24M, ""))[1:(24 - monthOffSet)]
+      
+      if(floor(difftime(TIMEL_LSTINQ, TIMEL_ISS, units = 'days')/30) == 24 + monthOffSet){
+        TIMEL_SRPRCD24M <- TIMEL_LSTINQ - months(24 + monthOffSet - 1)
+        day(TIMEL_SRPRCD24M) <- 1
+        end24MTime <- TIMEL_LSTINQ - months(monthOffSet)
+        TIMEL_LSTINQ <- end24MTime
+        day(TIMEL_ERPRCD24M) <- 30
+        frontStr <- '*'
+        
+        mapStratTime <- TIMEL_SRPRCD24M 
+        mapEndTime <- TIMEL_LSTINQ - months(24)
+        day(mapEndTime) <- 30
+        
+        middleStr <- map(mapStratTime, mapEndTime, )
+        
+        all24MStr = paste0(frontStr, middleStr, remained24MStr)
+        return(all24MStr)
+      } else if(floor(difftime(TIMEL_LSTINQ, TIMEL_ISS, units = 'days')/30) < 24 + monthOffSet){
+        frontStr <- rep('/', 24 + monthOffSet - floor(difftime(TIMEL_LSTINQ, TIMEL_ISS, units = 'days')/30))
+        
+        TIMEL_SRPRCD24M <- TIMEL_LSTINQ - months(24 + monthOffSet - 1)
+        day(TIMEL_SRPRCD24M) <- 1
+        end24MTime <- TIMEL_LSTINQ - months(monthOffSet)
+        TIMEL_ERPRCD24M <- end24MTime
+        day(TIMEL_ERPRCD24M) <- 30
+        
+        mapStratTime <- TIMEL_SRPRCD24M
+        mapEndTime <- TIMEL_LSTINQ - months(monthOffSet)
+        middleStr <- map()
+        
+        all24MStr = paste0(frontStr, middleStr, remained24MStr)
+        return(all24MStr)
+      } else if(floor(difftime(TIMEL_LSTINQ,TIMEL_ISS, units = 'days')/30) > 24 + monthOffSet){
+        frontStr <- NULL
+        
+        TIMEL_SRPRCD24M <- TIMEL_LSTINQ - months(24 + monthOffSet - 1)
+        day(TIMEL_SRPRCD24M) <- 1
+        end24MTime <- TIMEL_LSTINQ - months(monthOffSet)
+        TIMEL_ERPRCD24M <- end24MTime
+        day(TIMEL_ERPRCD24M) <- 30
+        
+        mapStratTime <- TIMEL_SRPRCD24M + months(1)
+        mapEndTime <- TIMEL_LSTINQ - months(monthOffSet)
+        middleStr <- map()
+        
+        all24MStr = paste0(frontStr, middleStr, remained24MStr)
+        
+      }
+    }
+  }
+}
